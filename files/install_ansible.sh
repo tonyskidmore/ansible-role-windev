042375e9da2bc9617f1621fb832a38f2932cd3bb
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2021-12-19 14:06:56 +0000

    splitting ansible install

--- files/install_ansible.sh
+++ files/install_ansible.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
 pipx install ansible
---
+++ files/install_pipx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
