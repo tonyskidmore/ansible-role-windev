fd744ce7832374161e4ef3bc059f3eff52c01ce0
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-01-22 13:38:50 +0000

    adding ssh kley handling

--- files/install_ansible.sh
+++ files/install_ansible.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-/home/adminuser/.local/bin/pipx install ansible --include-deps
+/home/adminuser/.local/bin/pipx install ansible[azure] --include-deps
+
+# put SSH private key file in place
+if [[ -f /mnt/c/ProgramData/ansible/id_rsa ]]
+then
+  mkdir -p ~/.ssh
+  chmod 0700 ~/.ssh
+  cp /mnt/c/ProgramData/ansible/id_rsa ~/.ssh/id_rsa
+  chmod 0600 ~/.ssh/id_rsa
+else
+  echo "/mnt/c/ProgramData/ansible/id_rsa"
+fi
+
