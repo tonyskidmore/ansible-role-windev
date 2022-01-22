556b7fc854e555a50c0618608d778d3d7da93c38
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-01-22 14:11:04 +0000

    adding ansible content

--- files/install_ansible.sh
+++ files/install_ansible.sh
@@ -13,3 +13,15 @@ else
   echo "/mnt/c/ProgramData/ansible/id_rsa"
 fi
 
+mkdir -p ~/ansible
+files=(inventory ansible.cfg linux.yml)
+for file in "${files[@]}"
+do
+    if [[ -f "/mnt/c/ProgramData/ansible/${file}" ]]
+    then
+      echo "Copying /mnt/c/ProgramData/ansible/${file}"
+      cp "/mnt/c/ProgramData/ansible/${file}" ~/ansible
+    else
+      echo "/mnt/c/ProgramData/ansible/${file} does not exist"
+    fi
+done
