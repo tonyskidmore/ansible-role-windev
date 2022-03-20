be7d8d76279fbd9cfce0fd62ce4facb17848e172
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-03-20 17:25:30 +0000

    updating podman install

--- files/install_podman.sh
+++ files/install_podman.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-mkdir -p ~/ansible
+# mkdir -p ~/ansible
 
-~/.local/bin/ansible-galaxy install diodonfrost.podman
+# ~/.local/bin/ansible-galaxy install diodonfrost.podman
 
-# prepare ansible configuration file
-cat > ~/ansible/podman.yml <<EOF
----
-- name: Install podman
-  hosts: localhost
-  connection: local
+# # prepare ansible configuration file
+# cat > ~/ansible/podman.yml <<EOF
+# ---
+# - name: Install podman
+#   hosts: localhost
+#   connection: local
 
-  roles:
-     - role: diodonfrost.podman
-       become: yes
-EOF
+#   roles:
+#      - role: diodonfrost.podman
+#        become: yes
+# EOF
 
-~/.local/bin/ansible-playbook $HOME/ansible/podman.yml
+# ~/.local/bin/ansible-playbook $HOME/ansible/podman.yml
+
+# TODO: put logic here
+sudo apt-get -y update
+sudo apt-get -y install podman
 
 ~/.local/bin/pipx install podman-compose
